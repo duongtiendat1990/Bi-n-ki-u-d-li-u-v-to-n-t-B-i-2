5fe023771945fc79045307ee51fd59805401dc99
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-03-27 11:33:58 +0700

    Bài 2

---
+++ script.js
@@ -0,0 +1,4 @@
+let Celsius = parseInt(prompt('Input Celsius Degree',));
+let Fahrenheit = Celsius * 9 / 5 +32
+document.write("Celsius Degree is " + Celsius); document.write('</br>');
+document.write("Fahrenheit Degree is " + Fahrenheit);
